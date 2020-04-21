15e7526e8bbe9b803374d2f1a29d0c531f893a0a
Adil Naimi <adilnm23@gmail.com>
2020-04-20 21:06:21 -0500

    change cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
     allow do
-      origins 'https://buy-and-sell-frontend.herokuapp.com'
+      origins 'http://localhost:3000'
     
       resource '*',
         headers: :any,
